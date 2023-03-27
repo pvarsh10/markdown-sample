e2ed3e87ff616eb575024d5437fb7cd4d9fde1cd
varshcodes 61 <93462387+pvarsh10@users.noreply.github.com>
2023-03-27 02:46:14 +0000

    added result page css

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
+import requests
+
 
 app=Flask(__name__)
 
 @app.route("/")
 
-def title():
-  return "Markdown"
+def home():
+  return render_template('home.html')
+
 
 if __name__=="__main__":
   app.run('0.0.0.0',debug=True)
